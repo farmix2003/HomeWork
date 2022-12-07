d2600bff214b80bfb5b400d8b4076cb3c48142fd
farmix2003 <ffarrux386@gmail.com>
2022-12-07 09:23:47 +0200

    new Scripts

---
+++ UygaVazifa/script.js
@@ -0,0 +1,10 @@
+"use strict";
+let numOfSeries = prompt("Nechta serial kordingiz?","");
+let seriesDB = {
+ count: numOfSeries,
+ series: {},
+ actors: {},
+ geners: [],
+ privat: false,
+};
+console.log(seriesDB.count);
